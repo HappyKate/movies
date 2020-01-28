c9ad09f6d4eab4bbf22f4042888e990057104178
HappyKate <zlaaayya@gmail.com>
2020-01-28 19:19:15 +0500

    *

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default class App extends React.Component {
     render() {
 
         return (
-            <div className="App">
+            <div className="container">
                 {movies.map(el => <Films{...el}/>)}
                 <Films/>
             </div>
--- src/data/films.js
+++ src/data/films.js
@@ -2,15 +2,18 @@ import React, {Component} from "react";
 
 export default class Films extends Component{
     render() {
-        const {name, year, localized_name, rating, image_url}= this.props;
+        const {name, year, localized_name, rating}= this.props;
         return(
-            <div className={'card'}>
-                <img src={image_url} alt={'..'} className={'card-img-left'}/>
+            <div className={'card mt-3 md-3'}>
                 <div className="card-body">
-                    <h2 className={'card-title'}>{localized_name}</h2>
-                    <p className={'card-text'}>{name}</p>
                     <p>{year}</p>
-                    <p>{rating}</p>
+                    <div className={'col'}>
+                        <h5 className={'card-title'}>{localized_name}</h5>
+                        <p className={'card-text'}>{name}</p>
+                    </div>
+                    <div className={'col'}>
+                        <p>{rating}</p>
+                    </div>
                 </div>
             </div>
         )
