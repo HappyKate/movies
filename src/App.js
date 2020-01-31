2c523770de30d9f71c2b3330fc0651559a0c87b2
HappyKate <zlaaayya@gmail.com>
2020-01-31 14:54:44 +0500

    Добавила модальное окно

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 import "./App.css";
 import Film from "./components/film";
 import { fetchMovies } from "./api/movies";
@@ -30,13 +31,33 @@ function App() {
           </div>
         </div>
       ))}
-
-      {element && (
-        <div className="card">
-          <p>{element.year}</p>
-        </div>
-      )}
-      {element && <div onClick={() => setElement(null)}>Закрыть</div>}
+      <Modal isOpen={!!element} toggle={() => setElement(null)}>
+        <ModalHeader toggle={() => setElement(null)}>
+          {element && <p> {element.localized_name}</p>}
+        </ModalHeader>
+        <ModalBody>
+          {element && (
+            <>
+              <div className="row">
+                <div className="col-md-6">
+                  <img className="col" src={element.image_url} alt={"..."} />
+                </div>
+                <div className="col-md-6">
+                  <p className="text-muted">{element.name}</p>
+                  <p>Год: {element.year}</p>
+                  <p>Рейтинг: {element.rating}</p>
+                </div>
+              </div>
+              <p className="mt-4 md-4">{element.description}</p>
+            </>
+          )}
+        </ModalBody>
+        <ModalFooter>
+          <Button color="secondary" onClick={() => setElement(null)}>
+            Закрыть
+          </Button>
+        </ModalFooter>
+      </Modal>
     </div>
   );
 }
--- src/components/film.js
+++ src/components/film.js
@@ -6,14 +6,14 @@ function Film({ element }) {
     <div>
       <div className="card mt-3 md-3">
         <div className="card-body">
-          <div className="col">
-            <h6 className="card-title">
-              {localized_name}
-            </h6>
-            <p className="card-text">{name}</p>
-          </div>
-          <div className="col">
-            <p>{rating}</p>
+          <div className="row">
+            <div className="col">
+              <h6 className="card-title">{localized_name}</h6>
+              <p className="text-muted">{name}</p>
+            </div>
+            <div className="col">
+              <p className='text-right'>{rating}</p>
+            </div>
           </div>
         </div>
       </div>
